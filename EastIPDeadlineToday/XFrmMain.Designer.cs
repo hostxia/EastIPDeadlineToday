f0d13367d8f0e00e519a492b71646f01b179ebe0
Xiameng <Xiameng@PC-XIAMENG>
2017-03-20 09:59:42 +0800

    当天期限统计

---
+++ EastIPDeadlineToday/XFrmMain.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace EastIPDeadlineToday
+{
+    partial class XFrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.xgcResult = new DevExpress.XtraGrid.GridControl();
+            this.xgvResult = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.xsbSearch = new DevExpress.XtraEditors.SimpleButton();
+            this.layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+            this.layoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.xgcResult)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xgvResult)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // layoutControl1
+            // 
+            this.layoutControl1.Controls.Add(this.xgcResult);
+            this.layoutControl1.Controls.Add(this.xsbSearch);
+            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutControl1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.Root = this.layoutControlGroup1;
+            this.layoutControl1.Size = new System.Drawing.Size(592, 524);
+            this.layoutControl1.TabIndex = 0;
+            this.layoutControl1.Text = "layoutControl1";
+            // 
+            // xgcResult
+            // 
+            this.xgcResult.Location = new System.Drawing.Point(12, 38);
+            this.xgcResult.MainView = this.xgvResult;
+            this.xgcResult.Name = "xgcResult";
+            this.xgcResult.Size = new System.Drawing.Size(568, 474);
+            this.xgcResult.TabIndex = 4;
+            this.xgcResult.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.xgvResult});
+            this.xgcResult.Load += new System.EventHandler(this.xgcResult_Load);
+            // 
+            // xgvResult
+            // 
+            this.xgvResult.GridControl = this.xgcResult;
+            this.xgvResult.Name = "xgvResult";
+            this.xgvResult.OptionsBehavior.Editable = false;
+            this.xgvResult.OptionsFind.AlwaysVisible = true;
+            this.xgvResult.OptionsSelection.MultiSelect = true;
+            this.xgvResult.OptionsView.ColumnAutoWidth = false;
+            this.xgvResult.OptionsView.ShowAutoFilterRow = true;
+            this.xgvResult.OptionsView.ShowFooter = true;
+            // 
+            // xsbSearch
+            // 
+            this.xsbSearch.Location = new System.Drawing.Point(12, 12);
+            this.xsbSearch.Name = "xsbSearch";
+            this.xsbSearch.Size = new System.Drawing.Size(568, 22);
+            this.xsbSearch.StyleController = this.layoutControl1;
+            this.xsbSearch.TabIndex = 1;
+            this.xsbSearch.Text = "拉取数据";
+            this.xsbSearch.Click += new System.EventHandler(this.xsbSearch_Click);
+            // 
+            // layoutControlGroup1
+            // 
+            this.layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.layoutControlGroup1.GroupBordersVisible = false;
+            this.layoutControlGroup1.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1,
+            this.layoutControlItem2});
+            this.layoutControlGroup1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlGroup1.Name = "layoutControlGroup1";
+            this.layoutControlGroup1.Size = new System.Drawing.Size(592, 524);
+            this.layoutControlGroup1.TextVisible = false;
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.xsbSearch;
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(572, 26);
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem1.TextVisible = false;
+            // 
+            // layoutControlItem2
+            // 
+            this.layoutControlItem2.Control = this.xgcResult;
+            this.layoutControlItem2.Location = new System.Drawing.Point(0, 26);
+            this.layoutControlItem2.Name = "layoutControlItem2";
+            this.layoutControlItem2.Size = new System.Drawing.Size(572, 478);
+            this.layoutControlItem2.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem2.TextVisible = false;
+            // 
+            // XFrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 524);
+            this.Controls.Add(this.layoutControl1);
+            this.Name = "XFrmMain";
+            this.Text = "当天绝限查询工具";
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+            this.layoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.xgcResult)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xgvResult)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
+        private DevExpress.XtraEditors.SimpleButton xsbSearch;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+        private DevExpress.XtraGrid.GridControl xgcResult;
+        private DevExpress.XtraGrid.Views.Grid.GridView xgvResult;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
+    }
+}
+
---
+++ EastIPDeadlineToday/XFrmMain.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.Data;
+using EastIPDeadlineToday.DbUtility;
+
+namespace EastIPDeadlineToday
+{
+    public partial class XFrmMain : DevExpress.XtraEditors.XtraForm
+    {
+        //private string _sSql = @"select v.caseno,v.agent,v.targetdate,v.tr,v.dor,m.title from ALERTVIEW v,ALERTMETA m where v.alerttype = m.typeid(+) and v.dor <= 0";
+        private string _sSql = @"select v.caseno,m.title,v.targetdate,v.dor from ALERTVIEW v,ALERTMETA m where v.alerttype = m.typeid(+) and v.dor <= 0 order by v.dor asc";
+        public XFrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void xsbSearch_Click(object sender, EventArgs e)
+        {
+            xgcResult.DataSource = DbHelperOra.Query(_sSql).Tables[0];
+            xgvResult.BestFitColumns(true);
+            xgvResult.Columns[0].Summary.Add(SummaryItemType.Count);
+        }
+
+        private void xgcResult_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
